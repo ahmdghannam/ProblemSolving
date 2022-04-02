32a6a339642cb23b2e0366a48f324b038f4854eb
JameelSawafta <97611802+JameelSawafta@users.noreply.github.com>
2022-04-02 12:36:48 +0300

    Update ProblemSolving.java

--- src/bat/aaup/ProblemSolving.java
+++ src/bat/aaup/ProblemSolving.java
@@ -2,11 +2,52 @@ package bat.aaup;
 
 import java.util.ArrayList;
 import java.util.Random;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 public class ProblemSolving {
 
-    public static void main(String[] args) {
+    static void solve() throws Exception {
+        
+    }
 
+    static int scanInt() throws IOException {
+        return parseInt(scanString());
+    }
+
+    static long scanLong() throws IOException {
+        return parseLong(scanString());
+    }
+
+    static String scanString() throws IOException {
+        while (tok == null || !tok.hasMoreTokens()) {
+            tok = new StringTokenizer(in.readLine());
+        }
+        return tok.nextToken();
+    }
+
+    static BufferedReader in;
+    static PrintWriter out;
+    static StringTokenizer tok;
+
+    public static void main(String[] args) {
+        try {
+            in = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(System.out);
+            solve();
+            in.close();
+            out.close();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            exit(1);
+        }
     }
 
      // for array
@@ -36,15 +77,6 @@ public class ProblemSolving {
 
         System.out.println();
     }
-    public static void printArray(int[] a) {
-
-        for (int i = 0; i < a.length; i++) {
-            Integer y = a[i];
-            System.out.print(y + "\t");
-        }
-
-        System.out.println();
-    }
     static boolean isSorted(ArrayList<Integer> array) {
         for (int i = 0; i < array.size() - 1; i++) {
             if (array.get(i) > array.get(i + 1))
@@ -57,5 +89,4 @@ public class ProblemSolving {
             temp[i- start]= a[i];
         }
     }
-
 }
